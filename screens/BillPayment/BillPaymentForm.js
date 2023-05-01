611537795985c565b5c5f6a24978143db7c73f11
Ibtid <ibtid011@example.com>
2023-05-01 09:50:33 +0600

    push

--- screens/BillPayment/BillPaymentForm.js
+++ screens/BillPayment/BillPaymentForm.js
@@ -7,42 +7,52 @@ import {
   TouchableOpacity,
   View,
   BackHandler,
+  Dimensions,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ContextStore from '../../Context/CotnextStore';
 import dispatch from '../../dispatch/dispatch';
 import actions from '../../dispatch/actions';
-const BillPaymentForm = ({ navigation }) => {
-  console.log("Cash In Amount")
-  const {contextStore, setContextStore} = useContext(ContextStore)
-  const [items, setItems] = useState([])
-  const [formData, setFormData] = useState({})
-  const [item, setItem] = useState({
-    name: "",
-    qty: "",
-    price: ""
-  })
-  const [amount, setAmount] = useState(0)
-  useEffect(() => {
-    if(items.length === 0){
-        setAmount(0)
-    }
-    else{
-        let vAmount = 0
-        items.map(item => {
-            vAmount += parseFloat(item.qty) * parseFloat(item.price)
+import { ScrollView } from 'react-native-gesture-handler';
+import { colorPalete } from '../../styles/color';
 
-        })
-        setAmount(vAmount)
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+const BillPaymentForm = ({ navigation }) => {
+  console.log('Cash In Amount');
+  const { contextStore, setContextStore } = useContext(ContextStore);
+  const [items, setItems] = useState([]);
+  const [formData, setFormData] = useState({});
+  const [item, setItem] = useState({
+    name: '',
+    qty: '',
+    price: '',
+  });
+  const [amount, setAmount] = useState(0);
+  useEffect(() => {
+    if (items.length === 0) {
+      setAmount(0);
+    } else {
+      let vAmount = 0;
+      items.map((item) => {
+        vAmount += parseFloat(item.qty) * parseFloat(item.price);
+      });
+      setAmount(vAmount);
     }
-  },[items])
+  }, [items]);
   useEffect(() => {
     (async () => {
-        const response = await dispatch(actions.getBillPaymentEntity, {}, {}, contextStore.agent.token)
-        setContextStore({...contextStore, billPaymentEntity: response})
-        console.log(response)
-    })()
-  },[])
+      const response = await dispatch(
+        actions.getBillPaymentEntity,
+        {},
+        {},
+        contextStore?.agent?.token
+      );
+      setContextStore({ ...contextStore, billPaymentEntity: response });
+      console.log(response);
+    })();
+  }, []);
   return (
     <SafeAreaView
       style={{
@@ -51,56 +61,204 @@ const BillPaymentForm = ({ navigation }) => {
         justifyContent: 'space-between',
         alignItems: 'flex-start',
       }}>
-        <Text>{contextStore.billPaymentEntity.name}</Text>
-      <View style={styles.screen}>
-        {items.map(item => (
-            <Text>{item.name} {item.qty} x {item.price} = {parseFloat(item.price) * parseFloat(item.qty)} <TouchableOpacity onPress={() => {
-                setItems(items.filter(vItem => vItem.name !== item.name))
-            }}><Text>Delete</Text></TouchableOpacity></Text>
+      <ScrollView
+        style={{
+          height: windowHeight,
+          width: windowWidth,
+          padding: 20,
+        }}>
+        <View
+          style={{
+            display: 'flex',
+            alignitems: 'center',
+            flexDirection: 'row',
+            width: '100%',
+          }}>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Name</Text>
+            <TextInput
+              style={styles.input}
+              selectTextOnFocus={false}
+              placeholder='name'
+              onChangeText={(text) => {
+                setItem({ ...item, name: text });
+              }}
+              value={item.name}
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Quantity</Text>
+            <TextInput
+              style={styles.input}
+              placeholder='qty'
+              keyboardType='numeric'
+              onChangeText={(text) => {
+                if (!text) {
+                  text = 0;
+                }
+                setItem({ ...item, qty: parseFloat(text) });
+              }}
+              value={item.qty.toString()}
+            />
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Price</Text>
+            <TextInput
+              style={styles.input}
+              placeholder='price'
+              keyboardType='numeric'
+              onChangeText={(text) => {
+                if (!text) {
+                  text = 0;
+                }
+                setItem({ ...item, price: parseFloat(text) });
+              }}
+              value={item.price.toString()}
+            />
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            setItems([...items, { ...item }]);
+            setItem({
+              name: '',
+              qty: 0,
+              price: 0,
+            });
+          }}>
+          <Text style={{ color: 'white' }}>Add</Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            fontSize: 25,
+            marginTop: 30,
+            borderTopWidth: 1,
+            paddingTop: 40,
+            marginBottom: 40,
+          }}>
+          Items: {items.length}
+        </Text>
+        <View
+          style={{
+            display: 'flex',
+            alignitems: 'center',
+            flexDirection: 'row',
+            width: '100%',
+            marginBottom: 20,
+          }}>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Name</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Quantity</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.label}>Price</Text>
+          </View>
+        </View>
+        {items.map((item) => (
+          <View
+            style={{
+              display: 'flex',
+              alignitems: 'center',
+              flexDirection: 'row',
+              width: '100%',
+            }}>
+            <View style={{ flex: 1 }}>
+              <Text style={styles.label}>{item.name}</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text style={styles.label}>{item.qty}</Text>
+            </View>
+            <View style={{ flex: 1 }}>
+              <Text style={styles.label}>{item.price}</Text>
+            </View>
+          </View>
         ))}
-        <TextInput placeholder='name' onChangeText={(text) => {
-            setItem({...item, name: text})
-        }} value={item.name}></TextInput>
-        <TextInput placeholder='qty' keyboardType='numeric' onChangeText={(text) => {
-            if(!text){
-                text = 0
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            console.log(amount);
+            if (items.length === 0) {
+              return ToastAndroid.show(
+                'At least one item needed',
+                ToastAndroid.SHORT
+              );
             }
-            setItem({...item, qty: parseFloat(text)})
-        }} value={item.qty.toString()}></TextInput>
-        <TextInput placeholder='price' keyboardType='numeric' onChangeText={(text) => {
-            if(!text){
-                text = 0
+            setContextStore({ ...contextStore, amount, items, formData });
+            navigation.navigate('CashInChooseOption');
+          }}>
+          <Text style={{ color: 'white' }}>Create Bill</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      {/* <Text>{contextStore?.billPaymentEntity?.name}</Text>
+      <View style={styles.screen}>
+        {items.map((item) => (
+          <Text>
+            {item.name} {item.qty} x {item.price} ={' '}
+            {parseFloat(item.price) * parseFloat(item.qty)}{' '}
+            <TouchableOpacity
+              onPress={() => {
+                setItems(items.filter((vItem) => vItem.name !== item.name));
+              }}>
+              <Text>Delete</Text>
+            </TouchableOpacity>
+          </Text>
+        ))}
+        <TextInput
+          placeholder='name'
+          onChangeText={(text) => {
+            setItem({ ...item, name: text });
+          }}
+          value={item.name}></TextInput>
+        <TextInput
+          placeholder='qty'
+          keyboardType='numeric'
+          onChangeText={(text) => {
+            if (!text) {
+              text = 0;
             }
-            setItem({...item, price: parseFloat(text)})
-        }} value={item.price.toString()}></TextInput>
+            setItem({ ...item, qty: parseFloat(text) });
+          }}
+          value={item.qty.toString()}></TextInput>
+        <TextInput
+          placeholder='price'
+          keyboardType='numeric'
+          onChangeText={(text) => {
+            if (!text) {
+              text = 0;
+            }
+            setItem({ ...item, price: parseFloat(text) });
+          }}
+          value={item.price.toString()}></TextInput>
         <TextInput></TextInput>
         <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-                setItems([...items, {...item}])
-                setItem({
-                    name: "",
-                    qty: 0,
-                    price: 0
-                  })
-                  
-            }}>
-            <Text style={{ color: 'white' }}>Add</Text>
-          </TouchableOpacity>
-        {contextStore.billPaymentEntity.paymentForm.map(item => (
-            <>
+          style={styles.button}
+          onPress={() => {
+            setItems([...items, { ...item }]);
+            setItem({
+              name: '',
+              qty: 0,
+              price: 0,
+            });
+          }}>
+          <Text style={{ color: 'white' }}>Add</Text>
+        </TouchableOpacity>
+        {contextStore?.billPaymentEntity?.paymentForm.map((item) => (
+          <>
             <Text style={styles.label}>{item.fieldName}</Text>
-        <TextInput
-          style={styles.input}
-          // onChangeText={onChangeNumber}
-          // value={number}
-          onChangeText={(text) => {
-            setFormData({...formData, [item.fieldName]:text})
-          }}
-          placeholder={item.fieldName}
-          keyboardType={item.fieldType === "String" ? "default" : "numeric"}
-        />
-            </>
+            <TextInput
+              style={styles.input}
+              // onChangeText={onChangeNumber}
+              // value={number}
+              onChangeText={(text) => {
+                setFormData({ ...formData, [item.fieldName]: text });
+              }}
+              placeholder={item.fieldName}
+              keyboardType={item.fieldType === 'String' ? 'default' : 'numeric'}
+            />
+          </>
         ))}
         <Text style={styles.label}>Amount</Text>
         <TextInput
@@ -110,24 +268,27 @@ const BillPaymentForm = ({ navigation }) => {
           value={amount.toString()}
           placeholder='Amount'
           keyboardType='numeric'
-          editable={false} 
+          editable={false}
           selectTextOnFocus={false}
         />
         <View style={styles.alignLeftButton}>
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              console.log(amount)
-              if(items.length === 0){
-                return ToastAndroid.show("At least one item needed", ToastAndroid.SHORT)
+              console.log(amount);
+              if (items.length === 0) {
+                return ToastAndroid.show(
+                  'At least one item needed',
+                  ToastAndroid.SHORT
+                );
               }
-              setContextStore({...contextStore, amount, items, formData})
+              setContextStore({ ...contextStore, amount, items, formData });
               navigation.navigate('CashInChooseOption');
             }}>
             <Text style={{ color: 'white' }}>Create Bill</Text>
           </TouchableOpacity>
         </View>
-      </View>
+      </View> */}
     </SafeAreaView>
   );
 };
@@ -142,17 +303,17 @@ const styles = StyleSheet.create({
     padding: 30,
   },
   label: {
-    fontSize: 20,
+    fontSize: 16,
     marginBottom: 6,
   },
   input: {
-    width: '100%',
+    width: '80%',
 
     marginBottom: 12,
     borderWidth: 1,
-    padding: 5,
+    padding: 4,
     borderRadius: 5,
-    fontSize: 20,
+    fontSize: 16,
   },
   alignLeftButton: {
     width: '100%',
--- screens/BillPayment/BillPaymentWait.js
+++ screens/BillPayment/BillPaymentWait.js
@@ -1,77 +1,74 @@
 import {
-    StyleSheet,
-    Text,
-    TextInput,
-    ToastAndroid,
-    TouchableOpacity,
-    Image,
-    View,
-    BackHandler,
-  } from 'react-native';
-  import { SafeAreaView } from 'react-native-safe-area-context';
-  import React, { useState, useEffect, useContext } from 'react';
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  TouchableOpacity,
+  Image,
+  View,
+  BackHandler,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useEffect, useContext } from 'react';
 
 import { colorPalete } from '../../styles/color';
 import ContextStore from '../../Context/CotnextStore';
 
-
-  
-  const BillPaymentWait = ({ navigation, route }) => {
-    const {user} = route.params
-    const {contextStore,setContextStore} = useContext(ContextStore)
-    useEffect(() => {
-        contextStore.socket.on("complete", () => {
-          contextStore.socket.disconnect()
-            navigation.reset({
-                index: 0,
-                routes: [
-                  {
-                    name: 'Home',
-                  },
-                ],
-              })
-            })
-            return () => {
-                contextStore.socket.off("complete")
-            }
-    },[])
-    return (
-      <SafeAreaView
-        style={{
-          flex: 1,
-          width: '100%',
-          justifyContent: 'space-between',
-          alignItems: 'flex-start',
-        }}>
-        <View style={styles.screen}>
+const BillPaymentWait = ({ navigation, route }) => {
+  const { user } = route.params;
+  const { contextStore, setContextStore } = useContext(ContextStore);
+  useEffect(() => {
+    contextStore.socket.on('complete', () => {
+      contextStore.socket.disconnect();
+      navigation.reset({
+        index: 0,
+        routes: [
+          {
+            name: 'Home',
+          },
+        ],
+      });
+    });
+    return () => {
+      contextStore.socket.off('complete');
+    };
+  }, []);
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+      }}>
+      <View style={styles.screen}>
         <Text>Waiting For {user.name}'s Confirmation</Text>
-        </View>
-      </SafeAreaView>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    screen: {
-      height: '100%',
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 30,
-    },
-    button: {
-      width: '100%',
-      backgroundColor: colorPalete.secondatWhite,
-      marginBottom: 40,
-  
-      padding: 5,
-      borderRadius: 5,
-      fontSize: 20,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
-  export default BillPaymentWait;
-  
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30,
+  },
+  button: {
+    width: '100%',
+    backgroundColor: colorPalete.secondatWhite,
+    marginBottom: 40,
+
+    padding: 5,
+    borderRadius: 5,
+    fontSize: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default BillPaymentWait;
